1a97cd1f6eade3cb3b6db24097b232a4ff200238
Caobo <351339592@qq.com>
2016-09-24 13:12:41 +0800

    更新状态栏效果
    更改布局文件及活动文件，在活动文件中设置透明状态栏

--- app/src/main/java/com/caobo/drawerlayouttest/MainActivity.java
+++ app/src/main/java/com/caobo/drawerlayouttest/MainActivity.java
@@ -1,26 +1,35 @@
 package com.caobo.drawerlayouttest;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //实现侧滑菜单状态栏透明
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+
+
         //这里是替换官方的actionbar,这里继承的是AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
 
         //初始化控件
